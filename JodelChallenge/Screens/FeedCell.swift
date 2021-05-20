3ddeb49f9889826eb36afb3581529f00cdcee646
Gungor Basa <gungorbasa@gmail.com>
2021-05-21 01:44:59 +0300

    Integrate Nuke to FeedCell

--- JodelChallenge/Screens/FeedCell.swift
+++ JodelChallenge/Screens/FeedCell.swift
@@ -6,16 +6,14 @@
 //  Copyright © 2019 Jodel. All rights reserved.
 //
 
+import Nuke
 import UIKit
 
-class FeedCell : UICollectionViewCell {
-    
-    @IBOutlet weak var imageView: UIImageView!
-    
-    public func configure(with imageUrl : URL) {
-        if let data = try? Data(contentsOf: imageUrl) {
-            let image = UIImage(data: data)
-            imageView.image = image
-        }
-    }
+final class FeedCell : UICollectionViewCell {
+  @IBOutlet private weak var imageView: UIImageView!
+
+  public func configure(with imageUrl : URL) {
+    Nuke.cancelRequest(for: imageView)
+    Nuke.loadImage(with: imageUrl, into: imageView)
+  }
 }
