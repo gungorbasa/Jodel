233a78e41699970a32d47732127f5ca482c23729
Gungor Basa <gungorbasa@gmail.com>
2021-05-21 11:15:36 +0300

    Remove flickrAPI.h/m

--- JodelChallenge/Screens/FeedViewController.swift
+++ JodelChallenge/Screens/FeedViewController.swift
@@ -9,27 +9,20 @@
 import UIKit
 
 class FeedViewController : UICollectionViewController {
-    
-    var photos : [URL] = []
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        FlickrApi.fetchPhotos { [weak self] (responsePhotos, error) in
-            self?.photos = responsePhotos ?? []
-            DispatchQueue.main.async(execute: {
-                self?.collectionView?.reloadData()
-            })
-        }
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return photos.count
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FeedCell", for: indexPath) as! FeedCell
-        cell.configure(with: photos[indexPath.row])
-        return cell
-    }
+
+  var photos : [URL] = []
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return photos.count
+  }
+
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FeedCell", for: indexPath) as! FeedCell
+    cell.configure(with: photos[indexPath.row])
+    return cell
+  }
 }
